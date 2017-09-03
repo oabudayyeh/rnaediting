c4fae0a398061f0c873ca51b07eece162ac49bb8
Omar Abudayyeh <abudayye@broadinstitute.org>
2017-09-03 09:49:12 -0400

    update

--- src/sub_sample.sh
+++ src/sub_sample.sh
@@ -8,8 +8,8 @@ echo $reads > $1.reads
 
 if (( $reads > $3 ))
 then
-	seqtk sample -s100 $1 15000000 > $1.sub
-	seqtk sample -s100 $2 15000000 > $2.sub
+	seqtk sample -s100 $1 $3 > $1.sub
+	seqtk sample -s100 $2 $3 > $2.sub
 	mv $1.sub $1
 	mv $2.sub $2
 	echo "subsample complete"
